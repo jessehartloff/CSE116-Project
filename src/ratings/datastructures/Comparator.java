e0bb0d057778a754a965d19cd6d85d84578556a3
jessehartloff <hartloff@buffalo.edu>
2023-08-29 12:32:15 -0400

    Start code for the CSE116 project

---
+++ src/ratings/datastructures/Comparator.java
@@ -0,0 +1,8 @@
+package ratings.datastructures;
+
+public class Comparator<T> {
+    //return true if a comes before b and false otherwise
+    public boolean compare(T a, T b) {
+        return false;
+    }
+}
