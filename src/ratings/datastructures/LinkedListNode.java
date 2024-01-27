a9712c0aad894f1df7ae22e86e3c5ab88db2340c
jessehartloff <hartloff@buffalo.edu>
2024-01-27 10:05:43 -0500

    update starter code for Spring 2024

--- src/ratings/datastructures/LinkedListNode.java
+++ src/ratings/datastructures/LinkedListNode.java
@@ -22,47 +22,4 @@ public class LinkedListNode<T> {
         return this.next;
     }
 
-    public int size() {
-        if (this.next == null) {
-            return 1;
-        } else {
-            return 1 + this.next.size();
-        }
-    }
-
-    public String toString() {
-        String out = "";
-        out += this.value;
-        if (this.next != null) {
-            out += this.next.toString() + " ";
-        }
-        return out;
-    }
-
-    public LinkedListNode<T> getElement(T value) {
-        if (this.value.equals(value)) {
-            return this;
-        } else if (this.next == null) {
-            return null;
-        } else {
-            return this.next.getElement(value);
-        }
-    }
-
-    public void append(T value) {
-        if (this.next == null) {
-            this.next = new LinkedListNode<>(value, null);
-        } else {
-            this.next.append(value);
-        }
-    }
-
-    public void insert(T value, int loc) {
-        if (loc == 0) {
-            this.next = new LinkedListNode<>(this.value, this.next);
-            this.value = value;
-        } else {
-            this.next.insert(value, loc - 1);
-        }
-    }
 }
